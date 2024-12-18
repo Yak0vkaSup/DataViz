dfd6bcab8353c818119edf4eb61d64e97262669d
Yak0vkaSup <belkovyakov@gmail.com>
2024-12-18 15:55:01 +0100

    histogramme

--- src/pages/dashboard/layout.py
+++ src/pages/dashboard/layout.py
@@ -4,6 +4,7 @@ from .component_chart import ChartComponent
 from .component_pie import PieComponent
 from .component_histogramme import HistogrammeComponent
 from .component_location import LocationComponent
+
 def DashboardPage():
     return html.Div(
         children=[
@@ -12,13 +13,23 @@ def DashboardPage():
             html.Main(
                 children=[
                     LocationComponent(),
-                    ChartComponent(),
-                    PieComponent(),
-                    HistogrammeComponent(),
+                    html.Div(
+                        PieComponent(),
+                        style={'width': '80vw', 'margin': '0 auto'},  # Center and increase width
+                    ),
+                    html.Div(
+                        ChartComponent(),
+                        style={'width': '80vw', 'margin': '0 auto'},
+                    ),
+                    html.Div(
+                        HistogrammeComponent(),
+                        style={'width': '80vw', 'margin': '0 auto'},
+                    ),
                 ],
-                className='main-content'
+                className='main-content',
+                style={'display': 'flex', 'flexDirection': 'column', 'alignItems': 'center'},  # Center align all components
             ),
             Footer()
         ],
-        className='complex-page'
+        className='complex-page',
     )
