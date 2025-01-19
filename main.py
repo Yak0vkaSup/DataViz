00aa0723d621d03bd9430489d1ef95f610e338ad
IIIADRIENIII <136937257+IIIADRIENIII@users.noreply.github.com>
2025-01-19 14:33:13 +0100

    Main scripted adapted to run everything, file destination changed to data/cleaned

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 from src.utils.generate_map import ChoroplethMapGenerator
+from src.utils.preprocess_data import main as preprocess_main
+from src.utils.get_data import DataDownloader
 
 base_path = os.path.abspath('./data')
 
@@ -9,7 +11,21 @@ def make_map(pickle_file,geojson_file, destination):
     generator.generate_maps()
 
 if __name__ == "__main__":
+    url = 'https://files.data.gouv.fr/geo-dvf/latest/csv/2023/full.csv.gz'
+    regions_geojson_url = 'https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/regions.geojson'
+    departments_geojson_url = 'https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/departements.geojson'
+    communes_geojson_url = 'https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/communes.geojson'
 
-    make_map(os.path.join(base_path, 'prix_m2_par_departement.pkl'),os.path.join(base_path, 'departments.geojson'),"region")
-    make_map(os.path.join(base_path, 'prix_m2_par_commune.pkl'),os.path.join(base_path, 'communes.geojson'),"departement")
-    make_map(os.path.join(base_path, 'prix_m2_par_region.pkl'),os.path.join(base_path, 'regions.geojson'),"pays")
+
+    downloader = DataDownloader(url)
+    downloader.run()  # Download, unzip, clean, and save as pickle
+
+    downloader.load_geojson('regions', regions_geojson_url)
+    downloader.load_geojson('departments', departments_geojson_url)
+    downloader.load_geojson('communes', communes_geojson_url)
+
+    preprocess_main()
+
+    make_map(os.path.join(base_path,'cleaned', 'prix_m2_par_departement.pkl'),os.path.join(base_path,'cleaned', 'departments.geojson'),"region")
+    make_map(os.path.join(base_path,'cleaned', 'prix_m2_par_commune.pkl'),os.path.join(base_path,'cleaned', 'communes.geojson'),"departement")
+    make_map(os.path.join(base_path,'cleaned', 'prix_m2_par_region.pkl'),os.path.join(base_path,'cleaned', 'regions.geojson'),"pays")
