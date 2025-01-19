67b48ea1dc1f686771ef2b5e3a7934fa06a792a3
IIIADRIENIII <136937257+IIIADRIENIII@users.noreply.github.com>
2025-01-19 14:58:05 +0100

    Main scripted adapted to run app.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from src.utils.generate_map import ChoroplethMapGenerator
 from src.utils.preprocess_data import main as preprocess_main
 from src.utils.get_data import DataDownloader
+from src.app import create_app
 
 base_path = os.path.abspath('./data')
 
@@ -17,15 +18,19 @@ if __name__ == "__main__":
     communes_geojson_url = 'https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/communes.geojson'
 
 
-    downloader = DataDownloader(url)
-    downloader.run()  # Download, unzip, clean, and save as pickle
+    # downloader = DataDownloader(url)
+    # downloader.run()  # Download, unzip, clean, and save as pickle
+    #
+    # downloader.load_geojson('regions', regions_geojson_url)
+    # downloader.load_geojson('departments', departments_geojson_url)
+    # downloader.load_geojson('communes', communes_geojson_url)
+    #
+    # preprocess_main()
+    #
+    # make_map(os.path.join(base_path,'cleaned', 'prix_m2_par_departement.pkl'),os.path.join(base_path,'cleaned', 'departments.geojson'),"region")
+    # make_map(os.path.join(base_path,'cleaned', 'prix_m2_par_commune.pkl'),os.path.join(base_path,'cleaned', 'communes.geojson'),"departement")
+    # make_map(os.path.join(base_path,'cleaned', 'prix_m2_par_region.pkl'),os.path.join(base_path,'cleaned', 'regions.geojson'),"pays")
 
-    downloader.load_geojson('regions', regions_geojson_url)
-    downloader.load_geojson('departments', departments_geojson_url)
-    downloader.load_geojson('communes', communes_geojson_url)
-
-    preprocess_main()
-
-    make_map(os.path.join(base_path,'cleaned', 'prix_m2_par_departement.pkl'),os.path.join(base_path,'cleaned', 'departments.geojson'),"region")
-    make_map(os.path.join(base_path,'cleaned', 'prix_m2_par_commune.pkl'),os.path.join(base_path,'cleaned', 'communes.geojson'),"departement")
-    make_map(os.path.join(base_path,'cleaned', 'prix_m2_par_region.pkl'),os.path.join(base_path,'cleaned', 'regions.geojson'),"pays")
+    print("Launching app...")
+    app = create_app()
+    app.run_server(debug=True)
